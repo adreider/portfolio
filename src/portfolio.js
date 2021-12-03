f61d5e44134ddbd70cb0b17513985b44369f7aef
adreider <adreidereconomic@gmail.com>
2021-12-03 18:56:52 -0300

    update all data

--- src/portfolio.js
+++ src/portfolio.js
@@ -14,7 +14,8 @@ const greeting = {
   logo_name: "dri.lino ()",
   nickname: "adreider",
   full_name: "Adriano Lino",
-  subTitle: "Full Stack Developer, Open Source Enthusiast 🔥. Always learning.",
+  subTitle:
+    "Full Stack Developer Jr, Open Source Enthusiast 🔥. Always learning.",
   resumeLink:
     "https://drive.google.com/file/d/1l3Sjx3T6I82vm_8ZaqvrypwY7yHzJtto/view?usp=sharing",
   mail: "mailto:adreidereconomic@gmail.com",
@@ -197,18 +198,18 @@ const skills = {
 const degrees = {
   degrees: [
     {
-//       title: "Shantilal Shah Engineering College",
-//       subtitle: "Bachelor in Information Technology",
-//       logo_path: "ssec.png",
-//       alt_name: "SSEC",
-//       duration: "2021 - Present",
-//       descriptions: [
-//         "⚡ I'm currently pursuing my bachelors in Information Technology.",
-//         // "⚡ I have studied core subjects like Data Structures, DBMS, Networking, Security, etc.",
-//         // "⚡ I have also completed various online courses for Backend , Web , Mobile App Development, etc.",
-//         // "⚡ I have implemented several projects based on what I've leart under my Computer Engineering course. ",
-//       ],
-//       website_link: "http://www.ssgec.ac.in/",
+      title: "Shantilal Shah Engineering College",
+      subtitle: "Bachelor in Information Technology",
+      logo_path: "ssec.png",
+      alt_name: "SSEC",
+      duration: "2021 - Present",
+      descriptions: [
+        "⚡ I'm currently pursuing my bachelors in Information Technology.",
+        // "⚡ I have studied core subjects like Data Structures, DBMS, Networking, Security, etc.",
+        // "⚡ I have also completed various online courses for Backend , Web , Mobile App Development, etc.",
+        // "⚡ I have implemented several projects based on what I've leart under my Computer Engineering course. ",
+      ],
+      website_link: "http://www.ssgec.ac.in/",
     },
   ],
 };
@@ -224,6 +225,53 @@ const certifications = {
       alt_name: "Digital Innovation One",
       // color_code: "#2AAFED",
       color_code: "#fff",
+    },
+    // {
+    //   title: "Hackathon",
+    //   subtitle: "Hack The Mountains",
+    //   logo_path: "hackathon1.svg",
+    //   certificate_link:
+    //     "https://drive.google.com/file/d/1Y-HT1flWDfeyfnfqVr3ubNgGIcnuUEYt/view?usp=sharing",
+    //   alt_name: "hackathon",
+    //   color_code: "#E2405F",
+    // },
+    {
+      title: "A300: Atlas Security",
+      subtitle: "MongoDB University",
+      logo_path: "mongo.png",
+      certificate_link:
+        "https://drive.google.com/file/d/12u_tkvhgB-K2TIm-RDdJOwYbQ9ccqiqA/view?usp=sharing",
+      alt_name: "MongoDB University",
+      // color_code: "#F6B808",
+      color_code: "#47A048",
+    },
+    // {
+    //   title: "Cyber Security & Cyber Forensics",
+    //   subtitle: "Workshop at IIT Bombay",
+    //   logo_path: "iit.png",
+    //   certificate_link:
+    //     "https://drive.google.com/file/d/12uAdjQC5LfrB1ODdxqIY181ugyyQhl-I/view?usp=sharing",
+    //   alt_name: "Workshop",
+    //   color_code: "#2AAFED",
+    // },
+    // {
+    //   title: "MLH Local Hack Day: Build",
+    //   subtitle: "Major League Hacking",
+    //   logo_path: "mlh-logo.svg",
+    //   certificate_link:
+    //     "https://drive.google.com/file/d/1ws7g2Oepu76vDFir6SvR_emFAMSkB2ZQ/view?usp=sharing",
+    //   alt_name: "Google",
+    //   color_code: "#fe0037",
+    // },
+    {
+      title: "Postman Student Expert",
+      subtitle: "Postman",
+      logo_path: "postman.png",
+      certificate_link:
+        "https://badgr.com/public/assertions/h2qevHs4SlyoHErJQ0mn2g",
+      alt_name: "Postman",
+      // color_code: "#f36c3d",
+      color_code: "#fffbf3",
     },
     // color_code: "#8C151599",
     // color_code: "#7A7A7A",
@@ -259,6 +307,92 @@ const experience = {
           // "I worked on the Dashboard project which helps users track their activities while using Walo Application. I also worked on Ocean Inventory Application and it's Admin panel Backend as well as on Ocean Inventory Admin Front-end using React and also worked on Walo Admin Backend.",
           color: "#0071C5",
         },
+        // {
+        //   title: "Full Stack Developer (Freelancer)",
+        //   company: "NightOwls",
+        //   company_url: "http://nightowls.company/",
+        //   logo_path: "nightowls.jpg",
+        //   duration: "Sep 2020 - Oct 2020",
+        //   location: "Work From Home",
+        //   description:
+        //   `Work closely with the Client and Team on change request functions.
+        //   We Primarily used LAMP Stack. 40% of my work was doing backend development, 30% of my work was designing database schemas, and 30% of my work was on maintain hosting and user traffics on the website.
+        //   `,
+        //     // "Created Front end of Yearn Financial Mutual Funds website. also degined simple web application for better user experience, designed DB Schemas as well.",
+        //   color: "#ee3c26",
+        // }
+      ],
+    },
+    // {
+    //   title: "Volunteerships",
+    //   experiences: [
+    //     {
+    //       title: "Cross Winter of Code Mentor",
+    //       company: "CWOC",
+    //       company_url: "https://crosswoc.ieeedtu.in/",
+    //       logo_path: "cwoc.png",
+    //       duration: "Feb 2021 - Present",
+    //       location: "Work From Home",
+    //       description:
+    //         "Mentorship responsibilities were to help students plan the project, review issues and pull requests, ensure smooth progress and help them out when they are stuck.",
+    //       color: "#4285F4",
+    //     },
+    //     {
+    //       title: "Campus Hustler",
+    //       company: "Skillenza",
+    //       company_url: "https://skillenza.com/",
+    //       logo_path: "skillenza.png",
+    //       duration: "Feb 2021 - Present",
+    //       location: "Work from Home",
+    //       description: "Spread Awareness of new Technologies and new Opportunities to Students and Grow Skillenza Community.",
+    //       color: "#196acf",
+    //     },
+    //     {
+    //       title: "GitHub Student Developer",
+    //       company: "GitHub",
+    //       company_url: "https://github.com/",
+    //       logo_path: "github.png",
+    //       duration: "Nov 2019 - Present",
+    //       location: "Work from Home",
+    //       description:
+    //       "Contribute to Open Source Community and Open Source Project.",
+    //       color: "#040f26",
+    //     },
+    //     {
+    //       title: "Google Local Guide",
+    //       company: "Google Map",
+    //       company_url: "https://maps.google.com/localguides/",
+    //       logo_path: "localguide.png",
+    //       duration: "Sep 2018 - Present",
+    //       location: "Work From Home",
+    //       description:
+    //         "Day-to-day responsibilities of helping local businesses to spread their business to the world. Helping users by writing reviews about different locations and spaces such as shops, malls, etc.",
+    //       color: "#D83B01",
+    //     },
+    //     {
+    //       title: "GDG Student Volunteer",
+    //       company: "Google Developer Groups",
+    //       company_url: "https://gdg.community.dev/",
+    //       logo_path: "gdg.png",
+    //       duration: "Feb 2021 - Present",
+    //       location: "Work From Home",
+    //       description:
+    //         "Google Developer Group Surat Student Volunteer and Member.",
+    //       color: "#D83B01",
+    //     },
+    //     {
+    //       title: "E. F. I. Student Volunteer",
+    //       company: "ENVIRONMENTALIST FOUNDATION OF INDIA",
+    //       company_url: "https://indiaenvironment.org/",
+    //       logo_path: "efi.png",
+    //       duration: "Apr 2017 - Present",
+    //       location: "Work From Home",
+    //       description:
+    //         "Take time out over weekends to join a gang of nation builders, nature enthusiasts to conserve what is left.",
+    //       color: "#5a900f",
+    //     },
+    //   ],
+    // },
   ],
 };
 
@@ -274,7 +408,7 @@ const projectsHeader = {
 const contactPageData = {
   contactSection: {
     title: "Contact Me",
-    profile_image_path: "adriano.png",
+    profile_image_path: "adriano4.png",
     description:
       "You can contact me at the places mentioned below. I will try to get back to you as fast as I can. ",
   },
@@ -295,84 +429,288 @@ const projects = {
   data: [
     {
       id: "0",
-      name: "AutomateInstaPyBot",
-      url: "https://github.com/harikanani/AutomateInstaPyBot",
-      description:
-        "This is Instagram Bot. This will login to your Instagram account. Follow Users, Unfollow Users, Remove Profile Photo",
+      name: "Move.it",
+      url: "https://github.com/adreider/Move.it",
+      description: "Projeto desenvolvido na 4° edição da NLW | Rocketseat 💜",
       languages: [
         {
-          name: "Python",
-          iconifyClass: "logos-python",
+          name: "Reactjs",
+          iconifyClass: "logos-react",
         },
       ],
     },
     {
       id: "1",
-      name: "react-twitter-clone",
-      url: "https://github.com/harikanani/react-twitter-clone",
+      name: "podcastr",
+      url: "https://github.com/adreider/podcastr",
       description:
-        "A React Twitter Clone UI with basic functionality such as make a Tweet.Embedded Profile Tweets and Share on Twitter.",
+        "O podcastr é um app para reprodução de episódios em áudio do seu podcast favorito.",
       languages: [
-        {
-          name: "HTML5",
-          iconifyClass: "vscode-icons:file-type-html",
-        },
-        {
-          name: "CSS3",
-          iconifyClass: "vscode-icons:file-type-css",
-        },
         {
           name: "React",
           iconifyClass: "logos-react",
-        },
-        {
-          name: "Firebase",
-          iconifyClass: "logos-firebase",
         },
       ],
     },
     {
       id: "2",
-      name: "node-blockchain",
-      url: "https://github.com/harikanani/node-blockchain",
+      name: "DTmoney",
+      url: "https://github.com/adreider/dtmoney",
       description:
-        "A simple blockchain and cryptocurrency wallet implemented in Node.js and TypeScript (for learning purposes).",
+        "O DTmoney é um projeto para controlar as e saidas de dinheiro da sua conta.",
       languages: [
         {
-          name: "NodeJS",
-          iconifyClass: "logos-nodejs",
-        },
-        {
-          name: "TypeScript",
-          iconifyClass: "logos-typescript",
+          name: "React",
+          iconifyClass: "logos-react",
         },
       ],
     },
     {
       id: "3",
-      name: "top-crypto-gainers",
-      url: "https://github.com/harikanani/top-crypto-gainers",
+      name: "GitExporer",
+      url: "https://github.com/adreider/Github-Explorer",
       description:
-        "A top high price changed crypto coins wring 24 hoursA sound-classifier webapp made with ReactJS + TensorflowJS.",
+        "O GitExporer é um projeto para listar os seus repositórios do Github",
       languages: [
-        {
-          name: "HTML5",
-          iconifyClass: "vscode-icons:file-type-html",
-        },
-        {
-          name: "CSS3",
-          iconifyClass: "vscode-icons:file-type-css",
-        },
-        {
-          name: "JavaScript",
-          iconifyClass: "logos-javascript",
-        },
         {
           name: "ReactJS",
           iconifyClass: "logos-react",
         },
       ],
     },
+    {
+      id: "4",
+      name: "finAPI",
+      url: "https://github.com/adreider/finAPI",
+      description: "finAPI é projeto de API de finanças",
+      languages: [
+        {
+          name: "NodeJS",
+          iconifyClass: "logos-nodejs",
+        },
+        {
+          name: "JavaScript",
+          iconifyClass: "logos-javascript",
+        },
+      ],
+    },
+    {
+      id: "5",
+      name: "Uploaded",
+      url: "https://github.com/adreider/Upload-Nodejs",
+      description: "Uploaded é uma aplicação de uploade de imagens na AWS",
+      languages: [
+        {
+          name: "NodeJS",
+          iconifyClass: "logos-nodejs",
+        },
+        {
+          name: "JavaScript",
+          iconifyClass: "logos-javascript",
+        },
+      ],
+    },
+    {
+      id: "6",
+      name: "JobsCalc",
+      url: "https://github.com/adreider/Maratona-02",
+      description:
+        "JobsCalc é um projeto para calcular o valor de um freelancer por horas trabalhadas",
+      languages: [
+        {
+          name: "HTML5",
+          iconifyClass: "vscode-icons:file-type-html",
+        },
+        {
+          name: "CSS3",
+          iconifyClass: "vscode-icons:file-type-css",
+        },
+        {
+          name: "NodeJs",
+          iconifyClass: "logos-nodejs",
+        },
+        {
+          name: "JavaScript",
+          iconifyClass: "logos-javascript",
+        },
+        {
+          name: "EJS",
+          iconifyClass: "logos-ejs",
+        },
+      ],
+    },
+    {
+      id: "7",
+      name: "gameplay",
+      url: "https://github.com/adreider/gameplay",
+      description:
+        "Aplicativo para lhe ajudar a conectar-se e organiza o momento de diversão e jogar com os amigos.",
+      languages: [
+        {
+          name: "ReactNative",
+          iconifyClass: "logos-react",
+        },
+      ],
+    },
+    // {
+    //   id: "6",
+    //   name: "Automate Attendace",
+    //   url: "https://github.com/harikanani/Node_Python",
+    //   description: "Automation of Online Attendance using node js and python",
+    //   languages: [
+    //     {
+    //       name: "JavaScript",
+    //       iconifyClass: "logos-javascript",
+    //     },
+    //     {
+    //       name: "Python",
+    //       iconifyClass: "logos-python",
+    //     },
+    //   ],
+    // },
+    // {
+    //   id: "7",
+    //   name: "Automate Discord Bot",
+    //   url: "https://github.com/harikanani/AutomateDiscordBot",
+    //   description:
+    //     "A Discord Bot to send Automatic messages to serer channel in regular time difference.",
+    //   languages: [
+    //     {
+    //       name: "Python",
+    //       iconifyClass: "logos-python",
+    //     },
+    //     {
+    //       name: "Python Selenium",
+    //       iconifyClass: "logos-selenium",
+    //     },
+    //     {
+    //       name: "Chromium Browser",
+    //       iconifyClass: "openmoji-chromium",
+    //     },
+    //   ],
+    // },
+    // {
+    //   id: "8",
+    //   name: "Flask Blog",
+    //   url: "https://github.com/harikanani/flask_blog",
+    //   description: "A Simple Blog Web Application made using Flask Framework",
+    //   languages: [
+    //     {
+    //       name: "Python",
+    //       iconifyClass: "logos-python",
+    //     },
+    //     {
+    //       name: "Flask",
+    //       iconifyClass: "cib-flask",
+    //     },
+    //     {
+    //       name: "HTML5",
+    //       iconifyClass: "vscode-icons:file-type-html",
+    //     },
+    //     {
+    //       name: "CSS3",
+    //       iconifyClass: "vscode-icons:file-type-css",
+    //     },
+    //   ],
+    // },
+    // {
+    //   id: "9",
+    //   name: "Netflix top series",
+    //   url: "https://github.com/harikanani/netflix-top-series",
+    //   description: "List of Top Netflix Series which is deployed to vercel.",
+    //   languages: [
+    //     {
+    //       name: "JavaScript",
+    //       iconifyClass: "logos-javascript",
+    //     },
+    //     {
+    //       name: "ReactJS",
+    //       iconifyClass: "logos-react",
+    //     },
+    //     {
+    //       name: "HTML5",
+    //       iconifyClass: "logos-html-5",
+    //     },
+    //     {
+    //       name: "CSS3",
+    //       iconifyClass: "logos-css-3",
+    //     },
+    //   ],
+    // },
+    // {
+    //   id: "10",
+    //   name: "COVID-19 Tracker",
+    //   url: "https://github.com/harikanani/Covid19TrackerReact",
+    //   description:
+    //     "Simple Covid-19 Tracker made using React and deployed to Vercel.",
+    //   languages: [
+    //     {
+    //       name: "JavaScript",
+    //       iconifyClass: "logos-javascript",
+    //     },
+    //     {
+    //       name: "ReactJS",
+    //       iconifyClass: "logos-react",
+    //     },
+    //     {
+    //       name: "HTML5",
+    //       iconifyClass: "logos-html-5",
+    //     },
+    //     {
+    //       name: "CSS3",
+    //       iconifyClass: "logos-css-3",
+    //     },
+    //   ],
+    // },
+    // {
+    //   id: "11",
+    //   name: "Food Order Static Website",
+    //   url: "https://github.com/harikanani/food-order-website",
+    //   description:
+    //     "A simple static website related to food restaurants service. this is reasponsive as well.",
+    //   languages: [
+    //     {
+    //       name: "HTML5",
+    //       iconifyClass: "logos-html-5",
+    //     },
+    //     {
+    //       name: "CSS3",
+    //       iconifyClass: "logos-css-3",
+    //     },
+    //     {
+    //       name: "JavaScript",
+    //       iconifyClass: "logos-javascript",
+    //     },
+    //     {
+    //       name: "PHP",
+    //       iconifyClass: "logos-php",
+    //     },
+    //   ],
+    // },
+    // {
+    //   id: "12",
+    //   name: "NFT Launchpad",
+    //   url: "https://deliquescent-cents.000webhostapp.com/",
+    //   description: "NFT Launchpad crypto site portfolio",
+    //   languages: [
+    //     {
+    //       name: "HTML5",
+    //       iconifyClass: "logos-html-5",
+    //     },
+    //     {
+    //       name: "CSS3",
+    //       iconifyClass: "logos-css-3",
+    //     },
+    //     {
+    //       name: "JavaScript",
+    //       iconifyClass: "logos-javascript",
+    //     },
+    //     {
+    //       name: "ReactJS",
+    //       iconifyClass: "logos-react",
+    //     },
+    //   ],
+    // },
   ],
 };
 
